66453a32ae32a8b54ea22315788e10887790d27f
Phil McMinn <phil.mcminn@gmail.com>
2024-10-22 15:11:14 +0100

    Update tags.py

--- tags.py
+++ tags.py
@@ -1,64 +1,64 @@
 agent_based_modelling = {
-    "tag": "agent-based-modelling",
+    "id": "agent-based-modelling",
     "title": "Agent-Based Modelling",
 }
 autograding = {
-    "tag": "autograding",
+    "id": "autograding",
     "title": "Autograding Student Assignments",
 }
 database_testing = {
-    "tag": "database-testing",
+    "id": "database-testing",
     "title": "Database Testing",
 }
 flaky_tests = {
-    "tag": "flaky-tests",
+    "id": "flaky-tests",
     "title": "Flaky Tests",
 }
 mutation_analysis = {
-    "tag": "mutation-analysis",
+    "id": "mutation-analysis",
     "title": "Mutation Analysis",
 }
 oracles = {
-    "tag": "oracles",
+    "id": "oracles",
     "title": "Oracles",
 }
 pseudo_tested = {
-    "tag": "pseudo-tested-code",
+    "id": "pseudo-tested-code",
     "title": "Pseudo-Tested Code",
 }
 regression_testing = {
-    "tag": "regression-testing",
+    "id": "regression-testing",
     "title": "Regression Testing",
 }
 search_based_software_engineering = {
-    "tag": "search-based-software-engineering",
+    "id": "search-based-software-engineering",
     "title": "Search-Based Software Engineering",
 }
 search_based_test_generation = {
-    "tag": "search-based-test-generation",
+    "id": "search-based-test-generation",
     "title": "Search-Based Test Generation",
 }
 software_maintenance = {
-    "tag": "software-maintenance",
+    "id": "software-maintenance",
     "title": "Software Maintenance",
 }
 testability = {
-    "tag": "testability",
+    "id": "testability",
     "title": "Testability",
 }
 testability_transformation = {
-    "tag": "testability-transformation",
+    "id": "testability-transformation",
     "title": "Testability Transformation",
 }
 test_case_diversity = {
-    "tag": "test-case-diversity",
+    "id": "test-case-diversity",
     "title": "Test Case Diversity",
 }
 test_suite_maintenance = {
-    "tag": "test-suite-maintenance",
+    "id": "test-suite-maintenance",
     "title": "Test Suite Maintenance",
 }
 web_presentation_failures = {
-    "tag": "web-presentation-failures",
+    "id": "web-presentation-failures",
     "title": "Web Presentation Failures",
 }
